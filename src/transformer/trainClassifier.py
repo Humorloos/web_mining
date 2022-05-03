2275e3abc70722893746fb6d03b325c08d44ed85
Lukas <l.loos95@gmail.com>
2022-05-03 13:40:26 +0200

    feat(trainClassifier): adjust save_dir depending on tune/test

--- src/transformer/trainClassifier.py
+++ src/transformer/trainClassifier.py
@@ -1,18 +1,19 @@
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
-from ray import tune
-from ray.tune.integration.pytorch_lightning import TuneReportCheckpointCallback
 
+from src.constants.constants import TRANSFORMER_DIR
 from src.transformer.emoBert import EmoBERT
 
 
-def train_classifier(config, max_time, min_delta, patience, checkpoint_dir=None, tune=False):
+def train_classifier(config, max_time, min_delta, patience, checkpoint_dir=None, do_tune=False):
     # initialize model
     if checkpoint_dir:
         model = EmoBERT.load_from_checkpoint(checkpoint_dir / "checkpoint")
     else:
         model = EmoBERT(config=config)
+
+    save_dir = TRANSFORMER_DIR / 'trials'
 
     # callbacks
     callbacks = [EarlyStopping(
@@ -20,15 +21,18 @@ def train_classifier(config, max_time, min_delta, patience, checkpoint_dir=None,
         min_delta=min_delta,
         patience=patience,
         verbose=True)]
-    if tune:
+    if do_tune:
+        from ray import tune
+        from ray.tune.integration.pytorch_lightning import TuneReportCheckpointCallback
         callbacks.append(TuneReportCheckpointCallback(
             # todo: add validation accuracy
             {'loss': 'ptl/val_loss'},
             on='validation_end'))
+        save_dir = tune.get_trial_dir()
 
     # train model
     trainer = pl.Trainer(
-        logger=WandbLogger(save_dir=tune.get_trial_dir(), project="web_mining"),
+        logger=WandbLogger(save_dir=save_dir, project="web_mining"),
         callbacks=callbacks,
         # gpus=torch.cuda.device_count(),
         gpus=0,
