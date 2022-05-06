9d9bd36fa470b02dfebd9a71db6a7171f8b27704
Lukas <l.loos95@gmail.com>
2022-05-06 11:54:56 +0200

    refactor(train): extract DEFAULT_CONFIG into constants

--- src/transformer/train.py
+++ src/transformer/train.py
@@ -1,19 +1,7 @@
 """Script for testing model training"""
 
-import torch
-
-from src.constants.constants import MAX_WORKERS
+from src.constants.constants import DEFAULT_CONFIG
 from src.transformer.trainClassifier import train_classifier
 
-config = {
-    'data_source': 'crawl',
-    'batch_size_train': 16,
-    'num_workers': MAX_WORKERS,
-    'optimizer': torch.optim.AdamW,
-    'lr': 1e-2,
-    'weight_decay': 1e-3,
-    'dropout_prob': 0.5,
-}
-
 if __name__ == '__main__':
-    train_classifier(config, fine_tune='adapter')
+    train_classifier(DEFAULT_CONFIG, fine_tune='adapter')
