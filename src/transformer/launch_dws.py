96b1dce080d8d90da97909dd58830cc2b5e67b78
Lukas <l.loos95@gmail.com>
2022-05-20 12:45:05 +0200

    feat(launchDws): add script for scheduling tune execution on dws

---
+++ src/transformer/launch_dws.py
@@ -0,0 +1,40 @@
+import logging
+import os
+from subprocess import Popen, PIPE
+from time import sleep
+from io import StringIO
+
+import pandas as pd
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+# %%
+while True:
+    gpu_data = pd.read_csv(StringIO(
+        Popen(['nvidia-smi', '--query-gpu=utilization.gpu,memory.free', '--format=csv'], stdout=PIPE)
+            .communicate()[0]
+            .decode('utf-8')
+    )).rename(columns={'utilization.gpu [%]': 'gpu', ' memory.free [MiB]': 'memory'})
+    print(gpu_data)
+    gpu_data.gpu = gpu_data.gpu.str.rstrip(' %').astype('int32')
+    gpu_data.memory = gpu_data.memory.str.rstrip(' MiB').astype('int32')
+    idle_gpus = gpu_data[(gpu_data.gpu == 0) & (gpu_data.memory > 48e3)]
+    if idle_gpus.shape[0] > 0:
+        target_gpu = idle_gpus.index[0]
+        logging.info(f'Found GPU {target_gpu} to be idle, starting training on GPU {target_gpu}')
+        os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(target_gpu)
+        try:
+            from tune import *
+        except Exception as e:
+            while True:
+                logging.exception(e)
+                sleep(60*60*24*7)
+        break
+    else:
+        logging.info('All GPUs are occupied, sleeping for 5 minutes')
+        sleep(5 * 60)
