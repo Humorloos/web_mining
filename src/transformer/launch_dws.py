eceb89a20a010e6c9a12da04fae08287516c7412
Lukas <l.loos95@gmail.com>
2022-05-20 13:51:15 +0200

    feat(launchDws): add option to train on GPU with largest free memory

--- src/transformer/launch_dws.py
+++ src/transformer/launch_dws.py
@@ -22,19 +22,32 @@ while True:
     print(gpu_data)
     gpu_data.gpu = gpu_data.gpu.str.rstrip(' %').astype('int32')
     gpu_data.memory = gpu_data.memory.str.rstrip(' MiB').astype('int32')
-    idle_gpus = gpu_data[(gpu_data.gpu == 0) & (gpu_data.memory > 48e3)]
-    if idle_gpus.shape[0] > 0:
-        target_gpu = idle_gpus.index[0]
-        logging.info(f'Found GPU {target_gpu} to be idle, starting training on GPU {target_gpu}')
-        os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(target_gpu)
-        try:
-            from tune import *
-        except Exception as e:
-            while True:
-                logging.exception(e)
-                sleep(60*60*24*7)
-        break
-    else:
-        logging.info('All GPUs are occupied, sleeping for 5 minutes')
-        sleep(5 * 60)
+    # For training on GPU with largest free memory
+    target_gpu = gpu_data.sort_values(by='memory', ascending=False).index[0]
+    logging.info(f'Starting training on GPU f{target_gpu}')
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(target_gpu)
+    try:
+        from tune import *
+    except Exception as e:
+        while True:
+            logging.exception(e)
+            sleep(60*60*24*7)
+    break
+    # For waiting for some GPU to get idle
+    # idle_gpus = gpu_data[(gpu_data.gpu == 0) & (gpu_data.memory > 48e3)]
+    # if idle_gpus.shape[0] > 0:
+    #     target_gpu = idle_gpus.index[0]
+    #     logging.info(f'Found GPU {target_gpu} to be idle, starting training on GPU {target_gpu}')
+    #     os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    #     os.environ['CUDA_VISIBLE_DEVICES'] = str(target_gpu)
+    #     try:
+    #         from tune import *
+    #     except Exception as e:
+    #         while True:
+    #             logging.exception(e)
+    #             sleep(60*60*24*7)
+    #     break
+    # else:
+    #     logging.info('All GPUs are occupied, sleeping for 5 minutes')
+    #     sleep(5 * 60)
