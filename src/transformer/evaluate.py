c821f9afd143f6cd20e63bdad529d095e7034233
Lukas <l.loos95@gmail.com>
2022-05-21 10:35:40 +0200

    feat(evaluate): add gpu support

--- src/transformer/evaluate.py
+++ src/transformer/evaluate.py
@@ -1,21 +1,27 @@
+import os
+
 from pytorch_lightning import Trainer
 
-from constants import DATA_DIR
+from constants import DATA_DIR, MAX_GPUS
 from datasets.TestDataLoader import get_test_dataloader
 from emoBert import EmoBERT
 
-TRIAL_DIR_NAME = '2022-05-06_12.13_255b1c85'  # name of trial directory
-CHECKPOINT_NAME = 'checkpoint_epoch=1-step=4'  # name of model's checkpoint
+TUNE_RUN_DIR_NAME = '2022-05-21_00.24'
+TRIAL_DIR_NAME = '2022-05-21_02.27_ddebab2c'  # name of trial directory
+CHECKPOINT_NAME = 'checkpoint_epoch=0-step=936'  # name of model's checkpoint
 DATA_SOURCE = 'premade'  # dataset to use for evaluation (one of 'sst2', 'original', or 'premade')
+TARGET_GPU = '0'
 
 if __name__ == '__main__':
     # with hpyerparameter-optimization:
     # set TRIAL_DIR_NAME to name of ray-tune directory and CHECKPOINT_NAME to name of checkpoint directory
     model = EmoBERT.load_from_checkpoint(
-        DATA_DIR / 'transformer' / 'ray_results' / 'test' / TRIAL_DIR_NAME / CHECKPOINT_NAME / 'checkpoint')
+        DATA_DIR / 'transformer' / 'ray_results' / TUNE_RUN_DIR_NAME / TRIAL_DIR_NAME / CHECKPOINT_NAME / 'checkpoint')
     # without hyperparameter-optimization:
     # set TRIAL_DIR_NAME to training dir name and CHECKPOINT_NAME to name of checkpoint file
     # model = EmoBERT.load_from_checkpoint(
     #     DATA_DIR / 'transformer' / 'trials' / 'web_mining' / TRIAL_DIR_NAME / 'checkpoints' / CHECKPOINT_NAME)
-    trainer = Trainer()
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = TARGET_GPU
+    trainer = Trainer(gpus=MAX_GPUS)
     trainer.test(model, dataloaders=get_test_dataloader(model=model, source=DATA_SOURCE))
