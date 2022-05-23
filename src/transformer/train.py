033fe1e7d3dfebbf6e508e0eb028264e09e2dcd4
Lukas <l.loos95@gmail.com>
2022-05-23 19:54:02 +0200

    feat(train): add gpu support

--- src/transformer/train.py
+++ src/transformer/train.py
@@ -1,7 +1,11 @@
 """Script for testing model training"""
+import os
 
 from constants import DEFAULT_CONFIG
 from trainClassifier import train_classifier
 
 if __name__ == '__main__':
+    from utils import get_idle_gpus
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(get_idle_gpus()[0])
     train_classifier(DEFAULT_CONFIG)
