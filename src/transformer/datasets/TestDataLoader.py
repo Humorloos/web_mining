d01e304c8fda6bb7b1088de295f6adb2db9c68fa
Lukas <l.loos95@gmail.com>
2022-05-23 19:53:07 +0200

    feat(evaluate): add support for reddit test set

--- src/transformer/datasets/TestDataLoader.py
+++ src/transformer/datasets/TestDataLoader.py
@@ -13,17 +13,23 @@ def get_test_dataloader(model: EmoBERT, source: str):
     Gets the test dataloader for evaluating the transformer model on a specific dataset
     :param model: EmoBERT model to get the dataloader for
     :param source: test set to get the data loader for, must be one of 'original' for the author's test set, 'sst2' for
-    the SST-2 benchmark test set, or 'premade' for our own crawled test set
+    the SST-2 benchmark test set, 'premade' for our own crawled twitter test set, or 'reddit' for our crawled reddit
+    test set
     :return: Dataloader loading data from the provided source
     """
     if source == 'sst2':
         test_set = get_sst2_test_set()
     elif source == 'original':
-        test_set = EmoticonDataset('test')
+        test_set = EmoBertDataset(data=EmoticonDataset('test').data)
     elif source == 'premade':
-        test_set = EmoBertDataset(data=pd.read_csv(
-            DATA_DIR / 'premade_datasets' / 'evaluation_dataset_non_lemmatized.csv'
-        )[['prep_text', 'sentiment']].rename(columns={'prep_text': 'text', 'sentiment': 'polarity'}))
+        test_set = EmoBertDataset.from_crawled_dataset(
+            pd.read_csv(DATA_DIR / 'premade_datasets' / 'evaluation_dataset_non_lemmatized.csv')
+        )
+    elif source == 'reddit':
+        test_set = EmoBertDataset.from_crawled_dataset(pd.concat([
+            pd.read_csv(DATA_DIR / 'reddit' / f'{polarity}_reddit_annotated_.csv')
+            for polarity in ['negative', 'positive']
+        ]))
     else:
         raise ValueError("parameter 'source' must be one of 'original', 'sst2', or 'premade'")
     return DataLoader(
--- src/transformer/evaluate.py
+++ src/transformer/evaluate.py
@@ -2,16 +2,19 @@ import os
 
 from pytorch_lightning import Trainer
 
-from constants import DATA_DIR, TARGET_GPUS
+from constants import DATA_DIR
 from datasets.TestDataLoader import get_test_dataloader
 from emoBert import EmoBERT
 
-TUNE_RUN_DIR_NAME = '2022-05-21_00.24'
-TRIAL_DIR_NAME = '2022-05-21_02.27_ddebab2c'  # name of trial directory
-CHECKPOINT_NAME = 'checkpoint_epoch=0-step=936'  # name of model's checkpoint
-DATA_SOURCE = 'premade'  # dataset to use for evaluation (one of 'sst2', 'original', or 'premade')
+TUNE_RUN_DIR_NAME = '2022-05-21_19.18'
+TRIAL_DIR_NAME = '2022-05-21_21.23_8d02f3ce'  # name of trial directory
+CHECKPOINT_NAME = 'checkpoint_epoch=0-step=11388'  # name of model's checkpoint
+DATA_SOURCE = 'reddit'  # dataset to use for evaluation (one of 'sst2', 'original', 'premade', or 'reddit')
 
 if __name__ == '__main__':
+    # from utils import get_gpu_with_most_available_memory
+    # target_gpu = get_gpu_with_most_available_memory()
+    target_gpu = '2'
     # with hpyerparameter-optimization:
     # set TRIAL_DIR_NAME to name of ray-tune directory and CHECKPOINT_NAME to name of checkpoint directory
     model = EmoBERT.load_from_checkpoint(
@@ -21,6 +24,6 @@ if __name__ == '__main__':
     # model = EmoBERT.load_from_checkpoint(
     #     DATA_DIR / 'transformer' / 'trials' / 'web_mining' / TRIAL_DIR_NAME / 'checkpoints' / CHECKPOINT_NAME)
     os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-    os.environ['CUDA_VISIBLE_DEVICES'] = TARGET_GPUS[0]
+    os.environ['CUDA_VISIBLE_DEVICES'] = target_gpu
     trainer = Trainer(gpus=1)
     trainer.test(model, dataloaders=get_test_dataloader(model=model, source=DATA_SOURCE))
